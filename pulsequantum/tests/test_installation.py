e524239d5a3e04ed068051cf984251d533e8ed97
RasmusBC59 <69790525+RasmusBC59@users.noreply.github.com>
2020-09-29 18:45:26 +0200

    updated workflow/main.yml (#5)
    * Telemetry False
    
    * added function myadd
    
    * added test fot myadd
    
    * Update hello_world.py
    
    added funct description
    
    * Create mainbla.yml
    
    bla
    
    * Update main.yml
    
    fixed indention
    
    * Update main.yml
    
    changed path to environment.yml
    
    * Update main.yml
    
    conda init bash
    
    * Update main.yml
    
    source activate
    
    * Update main.yml
    
    more echo
    
    * Update main.yml
    
    echo "$(conda env list)"
    
    * Update main.yml
    
    deactivate base
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    source to conda
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    split in two
    
    * Update main.yml
    
    echo path
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    internet fix
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    setup-miniconda@v1.7.0
    
    * Update main.yml
    
    conda info
    
    * Update environment.yml
    
    added: pytest, pytest-cov, mypy
    
    * Update main.yml
    
    mypy
    
    * Update main.yml
    
    pytest
    
    * Update main.yml
    
    * Update environment.yml
    
    removed test re
    
    * Update main.yml
    
    pytest
    
    * Update main.yml
    
    Build The Docs
    
    * Update main.yml
    
    * Update main.yml
    
    comment out fail
    
    * Update main.yml
    
    * Update main.yml
    
    make nice
    
    * Update main.yml
    
    if: ${{succeededOrFailed()}}
    
    * Update main.yml
    
    export SPHINXOPTS
    
    * Update main.yml
    
    removed SuccededOrFailed
    
    * Update main.yml
    
    * Update main.yml
    
    make html
    
    * Update main.yml
    
    removed #
    
    * Pipins (#5)
    
    * added new workflow and action
    
    * added requirements
    
    * path to pytest and mypy
    
    * fix run mypy
    
    * fix yml syntax
    
    * added pytest
    
    * Update python-app.yml
    
    changed  pytest  path
    
    * Update python-app.yml
    
    echo pwd
    
    * Update python-app.yml
    
    echo some more
    
    * Update python-app.yml
    
    echo ls
    
    * Update python-app.yml
    
    test ->tests
    
    * Update python-app.yml
    
    * telemetry false
    
    * moved conftets
    
    * Delete conftest.py
    
    deleted conftest
    
    * Update python-app.yml
    
    try new pytest line
    
    * buld docomentation
    
    * added _static
    
    * added opencensus-ext-azure to requirments
    
    * install pulsequantum
    
    * do not test py36
    
    * pytest and htmlfast
    
    * echo pip list
    
    * action pulsequantum
    
    * Update python-app.yml
    
    * conda install pandoc
    
    * only py37
    
    * echo pandoc version
    
    * echo pandoc version
    
    * Update python-app.yml
    
    * Update python-app.yml
    
    pandoc action test 1
    
    * cleaning of unused lines
    
    * htmlfast -> html
    
    * Delete action.yml
    
    * dont conda install pandoc
    
    * removed pandoc from requirements
    
    * python-app.yml run on main

--- pulsequantum/tests/test_installation.py
+++ pulsequantum/tests/test_installation.py
@@ -5,8 +5,14 @@ This test file is included so that the tests can run immediately after the
 template creation of the repo
 """
 
-from pulsequantum.hello_world import greeter, hello_world
+from pulsequantum.hello_world import greeter, hello_world, myadd
 
 
 def test_greeter():
     assert greeter("name") == f"Hello, name!"
+	
+def test_myadd():
+	x = 2
+	y = 2
+	assert myadd(x,y) == x+y 
+
