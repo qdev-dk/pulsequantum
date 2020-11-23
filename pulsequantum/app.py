b5caaaad9745644235d2cc28fdc0c4adc8bd1fe8
RasmusBC59 <rbcmail@gmail.com>
2020-11-23 12:13:01 +0100

    added app.py to start the GUI

---
+++ pulsequantum/app.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from  PulseGUI import pulsetable
+
+
+
+
+
+def run():
+    if not QApplication.instance():
+        app = QApplication(sys.argv)
+    else:
+        app = QApplication.instance()
+    
+    #app.aboutToQuit.connect(app.deleteLater)
+    pulsetable(AWG = None)
+    app.exec_()    
+if __name__ == "__main__": 
+    run()
