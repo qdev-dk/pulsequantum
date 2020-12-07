2ceb2e046862ce7d93258676f62c4238f83968ac
RasmusBC59 <rbcmail@gmail.com>
2020-12-07 12:10:49 +0100

    added entry point (not realy ussefulle yet, we need acces to the AWG )

--- setup.py
+++ setup.py
@@ -38,4 +38,9 @@ setup(
         "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3.7",
     ],
+    entry_points={
+        "console_scripts": [
+            "pulsequantum = pulsequantum.app:run",
+        ],
+    }
 )
