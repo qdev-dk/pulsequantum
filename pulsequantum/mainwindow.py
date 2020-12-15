ad7ef00925a0b88044085bed2499da3bbba181c0
RasmusBC59 <rbcmail@gmail.com>
2020-12-15 19:15:51 +0100

    updated layout

--- pulsequantum/mainwindow.py
+++ pulsequantum/mainwindow.py
@@ -13,10 +13,11 @@ from pulsequantum.dftable import QTableWidgetDF
 from pathlib import Path
 from pulsequantum.annotateshape import annotateshape
 matplotlib.use('QT5Agg')
+from PyQt5.QtWidgets import QMainWindow, QAction, QMenu, QApplication
 
 
 
-class pulsetable(QMainWindow,Gelem):
+class pulsetable(QWidget, Gelem):
     """
     This is the GUI setup for the main window
 
@@ -29,24 +30,17 @@ class pulsetable(QMainWindow,Gelem):
 
     def __init__(self, AWG=None, nchans=2, nlines=3, corrDflag=0):
         super().__init__()
-        self.setGeometry(50, 50, 1100, 900)
+        self.setGeometry(50, 50, 1200, 700)
         self.setWindowTitle('Pulse Table Panel')
         self.mainwindow = pulsetable
-        self.statusBar()
         self._sequencebox = None
         self.AWG = AWG
         self.nchans = nchans
         self.nlines = nlines
-        self.corrDflag = corrDflag
-       
-        #self.setStyleSheet("QLabel {font: 11pt Arial}")
+        self.corrDflag = corrDflag    
         self.setStyleSheet("QLineEdit, QLabel, QPushButton,QComboBox {font: 10pt Arial}")
-        #self.setStyleSheet(" {font: 8pt Arial}")
-
-        
-        
-        #, "QLabel {font: 11pt Arial}")
         self.home()
+        self.layout
 
     def home(self):
 
@@ -61,12 +55,17 @@ class pulsetable(QMainWindow,Gelem):
             init_list = yaml.load(file, Loader=yaml.FullLoader)
 
         self.divch = list(init_list['dividers']['channels'].values())
-        awgcloc_init = init_list['awgcloc']
+        self.awgcloc_init = init_list['awgcloc']
+
+
+        
 
         # Set up initial pulse table
         table = QTableWidgetDF(self)
-        table.setGeometry(50, 100, 1000, 400)
         self.loadElement(table, path=join(join(pathlib.Path(__file__).parents[0], 'initfiles/init.json')))
+        
+
+
 
         # Divider wiget
         win_divider = QWidget(self)
@@ -85,15 +84,15 @@ class pulsetable(QMainWindow,Gelem):
         divbtn = QPushButton('Set Dividers', self)
         divbtn.clicked.connect(lambda state: self.setDividers(chbox))
         lay_divider.addWidget(divbtn, 2, 0, 1, 4)
-        win_divider.move(100, 520)
-        win_divider.resize(win_divider.minimumSizeHint())
-        #win_divider.resize(200, 100) 
+
+        
+        
 
         # AWG clock ("sample rate")
         win_AWGclock = QWidget(self)
         lay_AWGclock = QGridLayout(win_AWGclock)
         setawgclockbox = QLineEdit(self)
-        setawgclockbox.setText(awgcloc_init)
+        setawgclockbox.setText(self.awgcloc_init)
         setawgclocklabel = QLabel(self)
         setawgclocklabel.setText('AWG Clock (GS/s)')
         setawgclocklabel.resize(setawgclocklabel.sizeHint())
@@ -103,133 +102,180 @@ class pulsetable(QMainWindow,Gelem):
         lay_AWGclock.addWidget(setawgclocklabel, 0, 0, 2, 1)
         lay_AWGclock.addWidget(setawgclockbox, 1, 0, 1, 1)
         lay_AWGclock.addWidget(setawgclockbtn, 1, 1, 1, 1)
-        win_AWGclock.move(500, 520)
-        win_AWGclock.resize(win_AWGclock.minimumSizeHint()) 
         
-        # Absolute Marker
-        win = QWidget(self);
-        lay= QVBoxLayout(win);
-        lay.addStretch();
-        lay1= QHBoxLayout();
-        lay1.addStretch();
-        lay2= QHBoxLayout();
-        lay2.addStretch();
+        # Absolute Marker        
         absmarkerch=QComboBox(self);
         for i in range(len(chbox)): 
-            absmarkerch.addItem('CH%dM1'%(i+1));absmarkerch.addItem('CH%dM2'%(i+1));
-        absstart=QLineEdit(self);absstart.setText('0');absstart.resize(absstart.sizeHint());absstop=QLineEdit(self);absstop.setText('0');absstop.resize(absstop.sizeHint());
-        abssetbtn = QPushButton('Set (us)', self);absrembtn = QPushButton('Remove All', self);abssetbtn.clicked.connect(lambda state: self.absMarkerSet(absmarkerch,absstart,absstop));absrembtn.clicked.connect(lambda state: self.absMarkerRemove(absmarkerch))
-        lay.addWidget(absmarkerch);lay1.addWidget(absstart);lay1.addWidget(absstop);
-        lay2.addWidget(abssetbtn);lay2.addWidget(absrembtn);lay.addLayout(lay1);lay.addLayout(lay2);
-        win.move(700,530);
-        win.resize(win.sizeHint());
-        absmarkerbox = QCheckBox(self);absmarkerbox.move(830, 515);absmarkerbox.stateChanged.connect(lambda state: self.absMarkerWidget(absmarkerbox,win))
-        absmarkerboxlabel= QLabel(self);absmarkerboxlabel.setText('Absolute Marker');absmarkerboxlabel.move(720, 520);absmarkerboxlabel.resize(absmarkerboxlabel.sizeHint())
+            absmarkerch.addItem('CH%dM1'%(i+1))
+            absmarkerch.addItem('CH%dM2'%(i+1))
+        absstart=QLineEdit(self);
+        absstart.setText('0');
+        
+        absstop=QLineEdit(self);
+        absstop.setText('0');
+        abssetbtn = QPushButton('Set (us)', self);
+        absrembtn = QPushButton('Remove All', self);
+        abssetbtn.clicked.connect(lambda state: self.absMarkerSet(absmarkerch,absstart,absstop));
+        absrembtn.clicked.connect(lambda state: self.absMarkerRemove(absmarkerch))      
+        
+        win_absmarker = QWidget(self);      
+        lay_absmarker= QGridLayout(win_absmarker)
+        lay_absmarker.addWidget(absmarkerch, 0, 0)
+        lay_absmarker.addWidget(absstart, 1, 0)
+        lay_absmarker.addWidget(absstop, 1, 1)
+        lay_absmarker.addWidget(abssetbtn, 3, 0)
+        lay_absmarker.addWidget(absrembtn, 3, 1)
+
+        win_absmarkerbox = QWidget(self)
+        lay_absmarkerbox = QGridLayout(win_absmarkerbox)
+        absmarkerbox = QCheckBox(self);
+        absmarkerbox.stateChanged.connect(lambda state: self.absMarkerWidget(absmarkerbox,win_absmarker))
+        absmarkerboxlabel= QLabel(self);
+        absmarkerboxlabel.setText('Absolute Marker');
+        lay_absmarkerbox.addWidget(absmarkerboxlabel, 0, 0)
+        lay_absmarkerbox.addWidget(absmarkerbox, 0, 1)
+         
+
 
         # lib box
         libbox = QComboBox(self)
-        libbox.addItem("-Load From Lib-")
+        libbox.addItem(" -Load From Lib- ")
         for i in range(len(self.seq_files)):
             libbox.addItem(self.seq_files[i])
         save_to = QLineEdit(self)
         save_to.setText('enter file name')
         
         # This is the start of top left buttons
-        win_puls = QWidget(self);
-        lay_puls= QGridLayout(win_puls);        
+        win_puls = QWidget(self)
+        lay_puls = QGridLayout(win_puls)       
 
         #Plot Element
         plotbtn = QPushButton('Plot Element', self)
         #plotbtn.resize(plotbtn.sizeHint());plotbtn.move(185, 10)
         plotbtn.clicked.connect(lambda state:self.plotElement())
         
-        #Generate Element
-        runbtn = QPushButton('Generate Element', self);
-        #runbtn.resize(runbtn.sizeHint());runbtn.move(40, 10);
+        # Generate Element
+        runbtn = QPushButton('Generate Element', self)
+        # runbtn.resize(runbtn.sizeHint());runbtn.move(40, 10);
         runbtn.clicked.connect(lambda state: self.generateElement(table))
         
-        #Save Element
+        # Save Element
         savebtn = QPushButton('Save Element', self)
         savebtn.clicked.connect(lambda state:self.saveElement(join(self.libpath,save_to.text())))
         
-        #Load Element
+        # Load Element
         loadbtn = QPushButton('Load Element', self)
-        loadbtn.clicked.connect(lambda state: self.loadElement(table,path=join(self.libpath,libbox.currentText()) ))
-        
-        #Populate table from Sequence
-        table_from_seq = QPushButton('Element from Sequence', self)
-        table_from_seq.clicked.connect(lambda state: self.from_sequence(table, seq = self._sequencebox.gseq))
-        
-        lay_puls.addWidget(runbtn,0,0,1,1)
-        lay_puls.addWidget(plotbtn,0,1,1,1)
-        lay_puls.addWidget(savebtn,2,0,1,1)
-        lay_puls.addWidget(save_to,2,1,1,1)
-        lay_puls.addWidget(loadbtn,1,0,1,1)
-        lay_puls.addWidget(libbox,1,1,1,1)
-        lay_puls.addWidget(table_from_seq ,2,3,1,1)
-        win_puls.move(20,5)
-        win_puls.resize(win_puls.sizeHint())
-        
-        # This is the end of top left buttons
+        loadbtn.clicked.connect(lambda state: self.loadElement(table, path=join(self.libpath,libbox.currentText())))
+ 
+        lay_puls.addWidget(runbtn, 0, 0, 1, 1)
+        lay_puls.addWidget(plotbtn, 0, 1, 1, 1)
+        lay_puls.addWidget(savebtn, 2, 0, 1, 1)
+        lay_puls.addWidget(save_to, 2, 1, 1, 1)
+        lay_puls.addWidget(loadbtn, 1, 0, 1, 1)
+        lay_puls.addWidget(libbox, 1, 1, 1, 1)
 
-        #Add a channel
-        whichch=QComboBox(self);whichch.move(420,10);
+
+    
+        #Add  or Remove a channel
+        win_add_remove_channel = QWidget(self)
+        lay_add_remove_channel = QGridLayout(win_add_remove_channel)
+        whichch=QComboBox(self)
         for i in range(len(chbox)): 
-            whichch.addItem('CH%d'%(i+1));
+            whichch.addItem('CH%d'%(i+1))
         addchbtn = QPushButton('Add Channel', self)
         addchbtn.clicked.connect(lambda state: self.addChannel(table,whichch))
-        addchbtn.resize(addchbtn.sizeHint())
-        addchbtn.move(350, 40)
-        
-        #Remove a channel
+
         remchbtn = QPushButton('Remove Channel', self)
         remchbtn.clicked.connect(lambda state: self.remChannel(table,whichch))
-        remchbtn.resize(remchbtn.sizeHint())
-        remchbtn.move(470, 40)
-        
-        #Add a pulse
+
+        lay_add_remove_channel.addWidget(whichch, 0, 0, 0, 2)
+        lay_add_remove_channel.addWidget(addchbtn, 1, 0)
+        lay_add_remove_channel.addWidget(remchbtn, 1, 1)
+
+
+        # Add or remove a pulse
+        win_add_remove_pulse = QWidget(self)
+        lay_add_remove_pulse = QGridLayout(win_add_remove_pulse)
+
+        whichp = QLineEdit(self);whichp.setText('Set name')        
         addpbtn = QPushButton('Add Pulse', self)
-        whichp = QLineEdit(self);whichp.setText('Set name');whichp.setGeometry(720,15,70,20);
         addpbtn.clicked.connect(lambda state: self.addPulse(table,whichp))
-        addpbtn.resize(addpbtn.sizeHint())
-        addpbtn.move(660, 40)
-        
-        #Remove a pulse
         rempbtn = QPushButton('Remove Pulse', self)
         rempbtn.clicked.connect(lambda state: self.remPulse(table,whichp))
-        rempbtn.resize(rempbtn.sizeHint())
-        rempbtn.move(760, 40)
-        
-        #Rename a pulse
-        renamepbtn = QPushButton('Rename Pulse', self);renamepbtn.resize(renamepbtn.sizeHint());renamepbtn.move(910, 40)
-        oldpname = QLineEdit(self);oldpname.setText('Old name');oldpname.setGeometry(890,15,70,20);
-        newpname = QLineEdit(self);newpname.setText('New name');newpname.setGeometry(965,15,70,20);
+
+        lay_add_remove_pulse.addWidget(whichp, 0, 0, 1, 2)
+        lay_add_remove_pulse.addWidget(addpbtn, 1, 0)
+        lay_add_remove_pulse.addWidget(rempbtn, 1, 1)
+
+        # Rename a puls
+        win_rename = QWidget(self)
+        lay_rename = QGridLayout(win_rename)
+        renamepbtn = QPushButton('Rename Pulse', self);renamepbtn.resize(renamepbtn.sizeHint())
+        oldpname = QLineEdit(self);oldpname.setText('Old name')
+        newpname = QLineEdit(self);newpname.setText('New name')
         renamepbtn.clicked.connect(lambda state: self.renamePulse(table,oldpname,newpname))
-        
-        #Remove a pulse
-        rempbtn = QPushButton('Remove Pulse', self)
-        rempbtn.clicked.connect(lambda state: self.remPulse(table,whichp))
-        rempbtn.resize(rempbtn.sizeHint())
-        rempbtn.move(760, 40)
-        
+        lay_rename.addWidget(renamepbtn, 0, 0, 1, 2)
+        lay_rename.addWidget(oldpname, 1, 0)
+        lay_rename.addWidget(newpname, 1, 1)
+
         
         #Correction D
         corrbtn = QPushButton('Correction D', self)
         corrbtn.clicked.connect(lambda state: self.correctionD(table))
-        corrbtn.resize(corrbtn.sizeHint())
-        corrbtn.move(100, 700)
 
-         
+        # Sequence and upload
 
-        #Sequence and upload
         seqbtn = QPushButton('Upload Sequence', self)
         seqbtn.clicked.connect(lambda state:self.sequence())
-        seqbtn.resize(seqbtn.sizeHint())
-        seqbtn.move(400, 700)
 
         
+        # setting up the layout of the main window
+        #win_RT = QWidget(self)
+        #lay_RT = QGridLayout(win_RT)
+       # lay_RT.addWidget(win_puls,0,0)
+
+
+        win_RM = QWidget(self)
+        lay_RM = QVBoxLayout(win_RM)
+        lay_RM.addWidget(win_add_remove_channel)
+        lay_RM.addWidget(win_add_remove_pulse)
+        lay_RM.addWidget(win_rename)
+        lay_RM.addWidget(corrbtn)
+
+        win_LB = QWidget(self)
+        lay_LB = QGridLayout(win_LB)
+        lay_LB.addWidget(win_divider,0,0)
+        lay_LB.addWidget(win_AWGclock,1,0)
+
+        win_MB = QWidget(self)
+        lay_MB = QGridLayout(win_MB)
+        lay_MB.addWidget(win_absmarkerbox, 0, 0)
+        lay_MB.addWidget(win_absmarker, 1, 0)
+        win_RB = QWidget(self)
+        lay_RB = QGridLayout(win_RB)
+        lay_RB.addWidget(seqbtn)
+
+        mainlayout = QGridLayout()
+        self.setLayout(mainlayout)
+
+        mainlayout.setRowStretch(0, 1)
+        mainlayout.setRowStretch(1, 10)
+        mainlayout.setRowStretch(2, 1)
+        
+        mainlayout.setColumnStretch(0, 1)
+        mainlayout.setColumnStretch(1, 10)
+        mainlayout.setColumnStretch(2, 1)
+
+        mainlayout.addWidget(win_puls,0,0)
+        mainlayout.addWidget(table, 1, 0, 1, 2)
+        mainlayout.addWidget(win_RM, 1, 2, 1, 1)
+        mainlayout.addWidget(win_LB, 2, 0)
+        mainlayout.addWidget(win_MB,2,1)
+        mainlayout.addWidget(win_RB, 2, 2)
+
+
         self.show()
-        win.hide()
+        win_absmarker.hide()
         
         
     def addChannel(self,table,whichch):
@@ -246,18 +292,20 @@ class pulsetable(QMainWindow,Gelem):
             table.setItem(row,n+1, QTableWidgetItem("0"));
             table.setItem(row,n+2, QTableWidgetItem("0"));
     
-    def remChannel(self,table,whichch):
+    # TODO fix this function
+    def remChannel(self,table,whichch): 
         self.nchans=self.nchans-1;
         n=table.columnCount();
-        n=n-1;
+        n=n-1-2;
         for i in range(n):
+            print(str(table.horizontalHeaderItem(i).text()))
             temp=str(whichch.currentText());
             if str(table.horizontalHeaderItem(i).text())==temp:
-                table.removeColumn(i);
+               table.removeColumn(i);
             if str(table.horizontalHeaderItem(i).text())==temp+"M1":    
                 table.removeColumn(i);
             if str(table.horizontalHeaderItem(i).text())==temp+"M2":
-                table.removeColumn(i);
+               table.removeColumn(i);
     
     def addPulse(self,table,whichp,i=-1):
         self.nlines=self.nlines+1;
