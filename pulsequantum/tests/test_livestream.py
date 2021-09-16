b5d65d2d950bdfd46a0a042ebd135012d316d4bc
RasmusBC59 <rbcmail@gmail.com>
2021-09-16 12:56:57 +0200

    updated tests

--- pulsequantum/tests/test_livestream.py
+++ pulsequantum/tests/test_livestream.py
@@ -1,36 +1,68 @@
+import pytest
 import numpy as np
 import time
 import qcodes as qc
 import os
+import tempfile
 from pulsequantum.livestream.example_instruments import FilterInstrument
 from panel.io.server import StoppableThread
-import tempfile
 from pulsequantum.livestream.livestream import LiveStream
 from qcodes import initialise_or_create_database_at, \
     load_or_create_experiment
+from pulsequantum.livestream.videoinstrument import VideoInstrument
+
+@pytest.fixture
+def source_db_path(tmp_path):
+    source_db_path = os.path.join(tmp_path, 'source.db')
+    return source_db_path
+
+@pytest.fixture(scope="function")
+def source_db(source_db_path):
+    initialise_or_create_database_at(source_db_path)
+
+@pytest.fixture(scope="function")
+def set_up_station():
+    exp = load_or_create_experiment('for_test', sample_name='no sample')
+    yield
+    exp.conn.close()
 
 
-db_path = os.path.join(tempfile.gettempdir(),
-                       'data_access_example.db')
-initialise_or_create_database_at(db_path)
+@pytest.fixture
+def test_instrument():
+    test_instrument = FilterInstrument("test_instrument")
+    yield test_instrument
+    test_instrument.close()
 
-experiment = load_or_create_experiment(
-    experiment_name='alazar',
-    sample_name='alazar-sample')
+@pytest.fixture
+def video(test_instrument):
+    video = VideoInstrument(name='video',
+                            data_func=test_instrument.spectrum_and_noise.get,
+                            n_points=(121,121)
+                           )  
+    yield video
+    video.close()
 
-SC = qc.Station()
+@pytest.fixture(scope="function")
+def live(test_instrument, video):
+
+    controllers = {'phase_x': (test_instrument.phase_x,0.1,0),
+                   'phase_y': (test_instrument.phase_y,0.1,0)}
+
+    live = LiveStream(data_func=video.videoaverage,controllers=controllers, port=0, refresh_period=500)
+
+    yield live
+
+    live.video_mode_callback.stop()
+    live.video_mode_server.stop()
+
+def test_livestream(live):
+    assert type(live.video_mode_server) == StoppableThread
+    assert live.video_mode_server.is_alive()
 
 
-def test_livestream():
 
-    test_instrumment = FilterInstrument('test')
-    pi = np.pi
-    sliders = {'phase_x': (test_instrumment.phase_x, 0, pi, 0.1, 0),
-               'phase_y': (test_instrumment.phase_y, 0, pi, 0.1, 0)}
-    test = LiveStream(data_func=test_instrumment.spectrum, sliders=sliders)
-    time.sleep(5)
 
-    assert type(test.video_mode_server) == StoppableThread
-    assert test.video_mode_server.is_alive()
-    test.video_mode_callback.stop()
-    test.video_mode_server.stop()
+
+def test_nr_average(source_db, set_up_station,video):
+    assert 1 == 1
+
