6064b29cf3f636cec2f9608d0b023a0c1d955ce1
RasmusBC59 <rbcmail@gmail.com>
2021-09-23 12:35:20 +0200

    made axes into channels

--- pulsequantum/livestream/videoinstrument.py
+++ pulsequantum/livestream/videoinstrument.py
@@ -3,26 +3,34 @@ from qcodes import Parameter
 import numpy as np
 from qcodes.utils.validators import Numbers, Arrays
 from qcodes.instrument.base import Instrument
+from qcodes.instrument.channel import InstrumentChannel
 from qcodes.instrument.parameter import ParameterWithSetpoints, Parameter
+from typing import Any, Iterable, Tuple, Union
 
 class GeneratedSetPoints(Parameter):
     """
     A parameter that generates a setpoint array from start, stop and num points
     parameters.
     """
-    def __init__(self, startparam, stopparam, numpointsparam, *args, **kwargs):
+    def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._startparam = startparam
-        self._stopparam = stopparam
-        self._numpointsparam = numpointsparam
+        self.reset()
+
+    def set_raw(self, value: Iterable[Union[float, int]]) -> None:
+        self.sweep_array = value
 
     def get_raw(self):
-        return np.linspace(self._startparam(), self._stopparam(),
-                           self._numpointsparam())
-        
+        return self.sweep_array
+
+    def reset(self):
+        V_dc = self.instrument.V_dc.get()
+        V_start = self.instrument.V_start.get() + V_dc
+        V_stop = self.instrument.V_stop.get() + V_dc
+        nr = self.instrument.n_points.get()
+        self.sweep_array = np.linspace(V_start, V_stop, nr)
 
 class Video(ParameterWithSetpoints):
-    
+
     def __init__(self, data_func, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.data_func = data_func
@@ -33,7 +41,7 @@ class Video(ParameterWithSetpoints):
 
 
 class VideoAverage(ParameterWithSetpoints):
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         #self.nr_average = self.root_instrument.nr_average.get()
@@ -42,11 +50,9 @@ class VideoAverage(ParameterWithSetpoints):
 
     def get_raw(self):
         self.nr_average = self.root_instrument.nr_average.get()
-        #self.nr_average += 1.0
         self.data = ((self.nr_average-1)*self.data + self.data_func())/self.nr_average
         self.nr_average += 1.0
         self.root_instrument.nr_average.set(self.nr_average)
-        
         return self.data
 
     def reset_average(self):
@@ -57,81 +63,22 @@ class VideoAverage(ParameterWithSetpoints):
 class VideoInstrument(Instrument):
 
     def __init__(self, name, data_func, n_points,  **kwargs):
-        
+
         super().__init__(name, **kwargs)
         self.data_func = data_func
-
-        self.add_parameter('V_x_start',
-                           initial_value=0,
-                           unit='V',
-                           label='V_x start',
-                           vals=Numbers(-1,1),
-                           get_cmd=None,
-                           set_cmd=None)
-
-        self.add_parameter('V_x_stop',
-                           initial_value=0.1,
-                           unit='V',
-                           label='V_x stop',
-                           vals=Numbers(-1,1),
-                           get_cmd=None,
-                           set_cmd=None)
-
-        self.add_parameter('V_y_start',
-                           initial_value=0,
-                           unit='V',
-                           label='V_y start',
-                           vals=Numbers(-1,1),
-                           get_cmd=None,
-                           set_cmd=None)
-
-        self.add_parameter('V_y_stop',
-                           initial_value=0.1,
-                           unit='V',
-                           label='V_y stop',
-                           vals=Numbers(-1,1),
-                           get_cmd=None,
-                           set_cmd=None)
         
-        self.add_parameter('n_pointsx',
-                           unit='',
-                           initial_value=n_points[0],
-                           vals=Numbers(1,2e4),
-                           get_cmd=None,
-                           set_cmd=None)
-
-        self.add_parameter('n_pointsy',
-                           unit='',
-                           initial_value=n_points[1],
-                           vals=Numbers(1,2e4),
-                           get_cmd=None,
-                           set_cmd=None)
-            
-        self.add_parameter('V_axis_x',
-                           unit='V',
-                           label='V Axis X',
-                           parameter_class=GeneratedSetPoints,
-                           startparam=self.V_x_start,
-                           stopparam=self.V_y_stop,
-                           numpointsparam=self.n_pointsx,
-                           vals=Arrays(shape=(self.n_pointsx.get_latest,)))
-        
-        self.add_parameter('V_axis_y',
-                           unit='V',
-                           label='V Axis Y',
-                           parameter_class=GeneratedSetPoints,
-                           startparam=self.V_y_start,
-                           stopparam=self.V_y_stop,
-                           numpointsparam=self.n_pointsy,
-                           vals=Arrays(shape=(self.n_pointsy.get_latest,)))
-        
+        # Add the channel to the instrument
+        for i, dim in enumerate(['x', 'y']):
+            channel = VideoAxes(self, dim, dim, n_points[i])
+            self.add_submodule(dim, channel)
+     
         self.add_parameter('video',
                            unit='V',
-                           setpoints=(self.V_axis_x,self.V_axis_y),
+                           setpoints=(self.x.V_axis,self.y.V_axis),
                            label='Video',
                            parameter_class=Video,
                            data_func = self.data_func,
-                           vals=Arrays(shape=(self.n_pointsx.get_latest,self.n_pointsy.get_latest)))
+                           vals=Arrays(shape=(self.x.n_points.get_latest,self.y.n_points.get_latest)))
 
         self.add_parameter('nr_average',
                            initial_value=1,
@@ -143,7 +90,50 @@ class VideoInstrument(Instrument):
 
         self.add_parameter('videoaverage',
                            unit='V',
-                           setpoints=(self.V_axis_x,self.V_axis_y),
+                           setpoints=(self.x.V_axis,self.y.V_axis),
                            label='VideoAverage',
                            parameter_class=VideoAverage,
-                           vals=Arrays(shape=(self.n_pointsx.get_latest,self.n_pointsy.get_latest)))
+                           vals=Arrays(shape=(self.x.n_points.get_latest,self.y.n_points.get_latest)))
+
+            
+class VideoAxes(InstrumentChannel):
+    def __init__(self, parent: Instrument, name: str, channel: str, n_points,  **kwargs):
+        super().__init__(parent, name, **kwargs)
+        self.dim = channel
+        self.add_parameter('V_dc',
+                           initial_value=0,
+                           unit='V',
+                           label='V_'+self.dim+' dc',
+                           vals=Numbers(-1,1),
+                           get_cmd=None,
+                           set_cmd=None)
+
+        self.add_parameter('V_start',
+                           initial_value=0,
+                           unit='V',
+                           label='V_'+self.dim+' start',
+                           vals=Numbers(-1,1),
+                           get_cmd=None,
+                           set_cmd=None)
+
+        self.add_parameter('V_stop',
+                           initial_value=0.1,
+                           unit='V',
+                           label='V_'+self.dim+' stop',
+                           vals=Numbers(-1,1),
+                           get_cmd=None,
+                           set_cmd=None)
+        
+        self.add_parameter('n_points',
+                           unit='',
+                           initial_value=n_points,
+                           vals=Numbers(1,2e4),
+                           get_cmd=None,
+                           set_cmd=None)
+            
+        self.add_parameter('V_axis',
+                           unit='V',
+                           label='V Axis '+self.dim,
+                           parameter_class=GeneratedSetPoints,
+                           vals=Arrays(shape=(self.n_points.get_latest,)))
+        
