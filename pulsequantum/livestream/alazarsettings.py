8172de9b92b41d8abdcf858a53990d885d1865c0
RasmusBC59 <rbcmail@gmail.com>
2021-10-07 12:37:38 +0200

    add AlazarConfig

--- pulsequantum/livestream/alazarsettings.py
+++ pulsequantum/livestream/alazarsettings.py
@@ -3,6 +3,7 @@ import param
 
 class AlazarSettings(param.Parameterized):
     clock_source = param.ObjectSelector(objects=['INTERNAL_CLOCK', 'FAST_EXTERNAL_CLOCK', 'EXTERNAL_CLOCK_10MHz_REF'])
+    sample_rate =
     coupling1 = param.ObjectSelector(objects=['DC', 'AC'])
     coupling2 = param.ObjectSelector(objects=['DC', 'AC'])
 
@@ -48,42 +49,59 @@ class AlazarSettings(param.Parameterized):
     apply  = param.Action(lambda x: x, doc="""Record timestamp.""", precedence=0.7)
 
 
-def alazarconfig(alazar, seqmode=False,external_clock=False):
-    """
-    function for config of alazar
-    """
-    with alazar.syncing():
-        if external_clock:    
-            alazar.clock_source('EXTERNAL_CLOCK_10MHz_REF')
-            alazar.external_sample_rate(500_000_000)
-        else:
-            alazar.clock_source('INTERNAL_CLOCK')
-            alazar.sample_rate(1_000_000_000)
-        alazar.clock_edge('CLOCK_EDGE_RISING')
-        alazar.decimation(1)
-        alazar.coupling1('DC')
-        alazar.coupling2('DC')
-        #alazar.channel_range1(.4)
-        #alazar.channel_range2(.4)
-        #alazar.impedance1(50)
-        #alazar.impedance2(50)
-        alazar.trigger_operation('TRIG_ENGINE_OP_J')
-        alazar.trigger_engine1('TRIG_ENGINE_J')
-        alazar.trigger_source1('EXTERNAL')
-        alazar.trigger_slope1('TRIG_SLOPE_POSITIVE')
-        alazar.trigger_level1(140)
-        alazar.trigger_engine2('TRIG_ENGINE_K')
-        alazar.trigger_source2('DISABLE')
-        alazar.trigger_slope2('TRIG_SLOPE_POSITIVE')
-        alazar.trigger_level2(140)
-        alazar.external_trigger_coupling('DC')
-        alazar.external_trigger_range('ETR_2V5')
-        alazar.trigger_delay(0)
-        alazar.timeout_ticks(0)
-        if seqmode:
-            alazar.aux_io_mode('AUX_IN_TRIGGER_ENABLE') 
-            alazar.aux_io_param('TRIG_SLOPE_POSITIVE') 
-        else:
-            alazar.aux_io_mode('AUX_IN_AUXILIARY') 
-            alazar.aux_io_param('NONE') 
+
+class AlazarConfig():
+
+    def __init__(self, alazar):
+
+        self.alazar = alazar
+        self.settings = AlazarSettings()
+
+    def config(self):
+        """
+        function for config of alazar
+        """
+        with self.alazar.syncing():
+            self.alazar.clock_source(self.settings.clock_source)    
+            self.alazar.external_sample_rate(self.settings.external_sample_rate)
+            self.alazar.clock_edge(self.settings.clock_edge)
+            self.alazar.decimation(self.settings.decimation)
+            self.alazar.coupling1(self.settings.coupling1)
+            self.alazar.coupling2(self.settings.coupling2)
+            self.alazar.trigger_operation(self.settings.trigger_operation)
+            self.alazar.trigger_engine1(self.settings.trigger_engine1)
+            self.alazar.trigger_source1(self.settings.trigger_source1)
+            self.alazar.trigger_level1(self.settings.trigger_level1)
+            self.alazar.trigger_engine2(self.settings.trigger_engine2)
+            self.alazar.trigger_source2(self.settings.trigger_source2)
+            self.alazar.trigger_level2(self.settings.trigger_level2)
+            self.alazar.external_trigger_coupling(self.settings.external_trigger_coupling)
+            self.alazar.external_trigger_range(self.settings.external_trigger_range)
+            self.alazar.trigger_delay(self.settings.trigger_delay)
+            self.alazar.timeout_ticks(self.settings.timeout_ticks)
+            if self.settings:
+                self.alazar.aux_io_mode('AUX_IN_TRIGGER_ENABLE') 
+                self.alazar.aux_io_param('TRIG_SLOPE_POSITIVE') 
+            else:
+                self.alazar.aux_io_mode('AUX_IN_TRIGGER_ENABLE') 
+                self.alazar.aux_io_param('TRIG_SLOPE_POSITIVE') 
+
+    def get_settings(self):
+        self.settings.clock_source = self.alazar.clock_source()    
+        self.settings.external_sample_rate = self.alazar.external_sample_rate()
+        self.settings.clock_edge = self.alazar.clock_edge()
+        self.settings.decimation = self.alazar.decimation()
+        self.settings.coupling1 = self.alazar.coupling1()
+        self.settings.coupling2 = self.alazar.coupling2()
+        self.settings.trigger_operation = self.alazar.trigger_operation()
+        self.settings.trigger_engine1 = self.alazar.trigger_engine1()
+        self.settings.trigger_source1 = self.alazar.trigger_source1()
+        self.settings.trigger_level1 = self.alazar.trigger_level1()
+        self.settings.trigger_engine2 = self.alazar.trigger_engine2()
+        self.settings.trigger_source2 = self.alazar.trigger_source2()
+        self.settings.trigger_level2 = self.alazar.trigger_level2()
+        self.settings.external_trigger_coupling = self.alazar.external_trigger_coupling()
+        self.settings.external_trigger_range = self.alazar.external_trigger_range()
+        self.settings.trigger_delay = self.alazar.trigger_delay()
+        self.settings.timeout_ticks = self.alazar.timeout_ticks()
 
