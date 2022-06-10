646260ba8c9480f407c9d50edc31774a57b9cb3f
RasmusBC59 <rbcmail@gmail.com>
2022-06-10 10:10:08 +0200

    add tests for get_samples_pr_record

--- pulsequantum/tests/test_livestream.py
+++ pulsequantum/tests/test_livestream.py
@@ -12,6 +12,7 @@ from qcodes import initialise_or_create_database_at, \
 from pulsequantum.livestream.videoinstrument import VideoInstrument
 from qcodes.utils.dataset.doNd import do0d
 from qcodes.dataset.data_set import load_by_id
+from pulsequantum.livestream.sweepsettings import SweepConfig 
 
 @pytest.fixture
 def source_db_path(tmp_path):
@@ -39,7 +40,7 @@ def test_instrument():
 def video(test_instrument):
     video = VideoInstrument(name='video',
                             data_func=test_instrument.spectrum_and_noise.get,
-                            n_points=(121,121)
+                            n_points=(40, 2208)
                            )  
     yield video
     video.close()
@@ -50,28 +51,29 @@ def test_SC(video):
     station.add_component(video)
     yield station
 
-@pytest.fixture(scope="function")
+#@pytest.fixture(scope="function")
+@pytest.fixture
 def live(test_instrument, video):
 
     controllers = {'phase_x': (test_instrument.phase_x,0.1,0),
                    'phase_y': (test_instrument.phase_y,0.1,0)}
 
-    live = LiveStream(data_func=video.videoaverage,controllers=controllers, port=0, refresh_period=500)
+    live = LiveStream(video=video, controllers=controllers, port=0, refresh_period=500)
 
     yield live
 
     live.video_mode_callback.stop()
     live.video_mode_server.stop()
+    live.sweepsettings.sequencebuilder.close()
+    live.video.close()
+
 
 def test_livestream(live):
     assert type(live.video_mode_server) == StoppableThread
     assert live.video_mode_server.is_alive()
 
 
-
-
-
-def test_nr_average(source_db, set_up_station,video, test_SC,live):
+def test_nr_average(source_db, set_up_station, video, test_SC, live):
     video.videoaverage.reset_average()
     data = do0d(video.videoaverage)
     blabla =load_by_id(data[0].run_id)
@@ -79,7 +81,20 @@ def test_nr_average(source_db, set_up_station,video, test_SC,live):
     data = do0d(video.videoaverage)
     blabla =load_by_id(data[0].run_id)
     assert blabla.snapshot['station']['instruments']['video']['parameters']['nr_average']['value'] == 2
+    live.data_func.reset_average()
     live.measure(event=True)
     assert int(live.run_id_widget.value) == 3
-    
 
+
+def test_get_samples_pr_record(live):
+    sweepconfig = live.sweepsettings
+    sweepconfig.settings.fast_time = 4.8
+    sweepconfig.settings.samplingrate_on_alazar = 200000
+    sweepconfig.settings.delay_time = 0.75
+    assert sweepconfig.get_samples_pr_record() == 1152
+
+    sweepconfig = live.sweepsettings
+    sweepconfig.settings.fast_time = 1.8
+    sweepconfig.settings.samplingrate_on_alazar = 20000
+    sweepconfig.settings.delay_time = 0.75
+    assert sweepconfig.get_samples_pr_record() == 640
